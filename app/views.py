17d0651dbc450f09122a30120c0833f255b511ee
Glen Padua <glenpadua01@gmail.com>
2016-08-05 18:22:05 +0530

    Added first template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World!"
+    user ={"name" : "Glen"}
+    return render_template("index.html",
+                                            title = "Home",
+                                            user = user)
