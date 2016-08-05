19aecf0e9c54abb61c641e0654a54be8cadd51fd
Glen Padua <glenpadua01@gmail.com>
2016-08-05 20:04:44 +0530

    More jinja practice with conditional statements and loops

--- app/views.py
+++ app/views.py
@@ -5,7 +5,18 @@ from app import app
 @app.route('/index')
 
 def index():
-    user ={"name" : "Glen"}
+    user ={"name" : "Glen"} # fake user
+    posts = [   # fake array of posts
+            {
+                "author" : {"name" : "John"},
+                "body" : "Beautiful day in Kochi!"
+            },
+            {
+                "author" : {"name" : "Glen"},
+                "body" : "Suicide Squad FTW!"
+            }
+    ]
     return render_template("index.html",
                                             title = "Home",
-                                            user = user)
+                                            user = user,
+                                            posts = posts)
