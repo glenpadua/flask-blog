9de00ab6d782fdea9a44ec9cefb459956dd149bc
Glen Padua <glenpadua01@gmail.com>
2016-08-09 20:16:18 +0530

    Fun with forms

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 
 def index():
     user ={"name" : "Glen"} # fake user
@@ -20,3 +21,20 @@ def index():
                                             title = "Home",
                                             user = user,
                                             posts = posts)
+
+
+@app.route("/login", methods = ["GET", "POST"])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for Open ID='%s', remember_me=%s" %
+              (form.open_id.data, str(form.remember_me.data)))
+        return redirect("/index")
+    return render_template("login.html",
+                                            title = "Sign In",
+                                            form = form)
+
+
+
+
