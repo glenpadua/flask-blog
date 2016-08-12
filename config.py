ba57f3a2a1578792852100c3e2db58483368208a
Glen Padua <glenpadua01@gmail.com>
2016-08-12 11:13:13 +0530

    Open ID Login stuff

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "jon-snow-comes-back-to-life"
 
+OPENID_PROVIDERS = [
+    {"name": "Yahoo", "url": "https://me.yahoo.com"},
+    {"name": "AOL", "url": "http://openid.aol.com/<username>"},
+    {"name": "Flickr", "url": "http://www.flickr.com/<username>"},
+    {"name": "MyOpenID", "url": "https://www.myopenid.com"}]
